a5ec68918c148818cc2f95016395bc37805adc87
Abraham-maker <imnoteverywhere12@gmail.com>
2023-07-26 12:08:18 -0400

    configuracion del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import { styled } from 'styled-components'
+import { rootReducer } from './store/reducers'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Provider store={store}>
+      <GlobalContainer>
+
+      </GlobalContainer>
+    </Provider>
   )
 }
 
 export default App
+
+const GlobalContainer = styled.div`
+  margin:0;
+  padding:0;
+  font-family: 'Be Vietnam Pro', sans-serif;
+`
---
+++ src/store/reducers/auth.jsx
@@ -0,0 +1,12 @@
+
+const initialState = {
+    token: null,
+    userData: {},
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
